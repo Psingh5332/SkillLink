687ceae2c31d6a43a0bffdb2db66206bb35656f5
Jigisha2912 <jigisha2903@gmail.com>
2025-08-01 00:50:12 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -130,11 +130,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 app.UseCors("AllowReactApp");
 app.UseHttpsRedirection();
 
