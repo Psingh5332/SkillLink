c5237a65e229892ec9562cbb44c5ef564b95555d
RAghav30003 <raghavmahendru8@gmail.com>
2025-07-31 19:10:45 -0400

    Update Peoples.jsx

--- client-app/src/Pages/Dashboard/Peoples/Peoples.jsx
+++ client-app/src/Pages/Dashboard/Peoples/Peoples.jsx
@@ -49,6 +49,7 @@ function Peoples() {
                                                         <p className="text-muted mb-1">{People.userName}</p>
                                                         <div className="d-flex align-items-center gap-2">
                                                             <span>Expertise</span>
+                                                             <span>EMaster</span>
                                                             <span>Skills</span>
                                                         </div>
                                                     </div>
