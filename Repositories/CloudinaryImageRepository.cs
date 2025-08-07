9cda68bb446b1b6cf423d102704a4df3535190e9
Jigisha2912 <jigisha2903@gmail.com>
2025-08-07 23:24:07 +0530

    Update CloudinaryImageRepository.cs

--- Repositories/CloudinaryImageRepository.cs
+++ Repositories/CloudinaryImageRepository.cs
@@ -18,17 +18,7 @@ namespace SkillLink.Repositories
                 config.GetSection("CloudinarySettings")["ApiSecret"]
                 );
 
-            // var cloudName = config["CloudinarySettings:CloudName"];
-            // var apiKey = config["CloudinarySettings:ApiKey"];
-            // var apiSecret = config["CloudinarySettings:ApiSecret"];
-
-            // Console.WriteLine("===== Cloudinary Config on Render =====");
-            // Console.WriteLine("CloudName: " + cloudName);
-            // Console.WriteLine("ApiKey: " + apiKey);
-            // Console.WriteLine("ApiSecret: " + (string.IsNullOrEmpty(apiSecret) ? "NOT SET" : "SET"));
-            // Console.WriteLine("=======================================");
-        
-            // account = new Account(cloudName, apiKey, apiSecret);
+         
         }
        
 
@@ -49,38 +39,10 @@ namespace SkillLink.Repositories
 
             return null;
 
-             // try
-             //    {
-             //        var client = new Cloudinary(account);
-            
-             //        var uploadParams = new ImageUploadParams
-             //        {
-             //            File = new FileDescription(file.FileName, file.OpenReadStream()),
-             //            Folder = "skilllink_uploads"
-             //        };
-            
-             //        var uploadResult = await client.UploadAsync(uploadParams);
-            
-             //        if (uploadResult != null && uploadResult.StatusCode == HttpStatusCode.OK)
-             //        {
-             //            Console.WriteLine("Upload successful. URL: " + uploadResult.SecureUrl);
-             //            return uploadResult.SecureUrl.ToString();
-             //        }
-            
-             //        Console.WriteLine("Upload failed. StatusCode: " + uploadResult?.StatusCode);
-             //        Console.WriteLine("Error: " + uploadResult?.Error?.Message);
-            
-             //        return null;
-             //    }
-             //    catch (Exception ex)
-             //    {
-             //        Console.WriteLine("Exception during Cloudinary upload: " + ex.Message);
-             //        Console.WriteLine("StackTrace: " + ex.StackTrace);
-             //        return null;
-             //    }
-             }
          }
+      }
 }
+
 
 
 
