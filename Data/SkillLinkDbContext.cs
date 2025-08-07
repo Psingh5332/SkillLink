f8add901da11536983c56a3681e108969bdb4d26
Jigisha2912 <jigisha2903@gmail.com>
2025-08-07 17:43:09 +0530

    Add files via upload

--- Data/SkillLinkDbContext.cs
+++ Data/SkillLinkDbContext.cs
@@ -1,14 +1,46 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SkilLink.Model.Domain;
 using SkillLink.Model.Domain;
 
 namespace SkillLink.Data
 {
-    public class SkillLinkDbContext : DbContext
+    public class SkillLinkDbContext : IdentityDbContext<ApplicationUser>
     {
         public SkillLinkDbContext(DbContextOptions<SkillLinkDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var userRoleId = "cda8ec06-af61-4627-8c82-3b3de4ac1ae0";
+            var adminRoleId = "5c83c52e-9426-46a3-b5b2-a6a591ca39f1";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id=userRoleId,
+                    ConcurrencyStamp=userRoleId,
+                    Name="User",
+                    NormalizedName="User".ToUpper()
+
+                },
+                 new IdentityRole
+                {
+                    Id=adminRoleId,
+                    ConcurrencyStamp=adminRoleId,
+                    Name="Admin",
+                    NormalizedName="Admin".ToUpper()
+
+                }
+
+            };
+            //seeding in builder object
+            builder.Entity<IdentityRole>().HasData(roles);
         }
 
         public DbSet<Image> Images { get; set; }
